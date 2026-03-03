11a199dc00c2df44ef5054ba7138f44155ad3b92
Saurav mishra <mishrasaurav499@gmail.com>
2026-03-03 18:00:27 +0530

    minor changes

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('mongodb connected successfully');
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('✅ mongodb connected successfully');
     } catch (error) {
         console.log(error);
     }
